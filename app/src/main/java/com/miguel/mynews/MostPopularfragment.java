40ba9ff79d406b71fd88d55be0ff7489d8b61ab5
Miguel <m.ferreira511@laposte.net>
2019-10-29 15:29:46 +0100

    In XML most popular, implemented a progress bar.
    In package Utils, created class for an asynchtask and a handler.

--- app/src/main/java/com/miguel/mynews/MostPopularfragment.java
+++ app/src/main/java/com/miguel/mynews/MostPopularfragment.java
@@ -1,41 +1,116 @@
 package com.miguel.mynews;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.miguel.mynews.Adapter.InfoCellAdapter;
 import com.miguel.mynews.Models.MostPopular;
+import com.miguel.mynews.Utils.CellInformationCalls;
+import com.miguel.mynews.Utils.MyAsyncTask;
+import com.miguel.mynews.Utils.MyHandlerThread;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class MostPopularfragment extends Fragment {
+public class MostPopularfragment extends Fragment implements CellInformationCalls.Callbacks, MyAsyncTask.Listeners {
 
-    public static TopStoriesfragment newInstance() {
-        TopStoriesfragment frag1 = new TopStoriesfragment();
+    public String KEYLIST = "mostpopularArrayList";
+    private MyHandlerThread handlerThread;
+    private ProgressBar progressBar;
+
+    public static MostPopularfragment newInstance() {
+        MostPopularfragment frag1 = new MostPopularfragment();
         return(frag1);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View result = inflater.inflate(R.layout.tab_fragment1, container, false);
+        View result = inflater.inflate(R.layout.mostpopular, container, false);
         RelativeLayout rootView = result.findViewById(R.id.relativelayout);
-        Bundle bundle = new Bundle();
-        ArrayList<String> mostpopularlist = bundle.getStringArrayList("mostpopularArrayList");
+        this.progressBar = result.findViewById(R.id.mostpop_progress_bar);
+        this.configureHandlerThread();
+        startHandlerThread();
+        executeHttpRequestWithRetrofit();
+
+            Bundle bundle = new Bundle();
+            List<MostPopular> mostpopularlist = (List<MostPopular>) bundle.getSerializable("KEYLIST");
+
+
+            InfoCellAdapter mondapteur;
+            RecyclerView recyclerView = result.findViewById(R.id.recycleview_view);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+            mondapteur = new InfoCellAdapter(mostpopularlist);
+            recyclerView.setAdapter(mondapteur);
+
         return result;
+    }
+    private void startAsyncTask() {
+        new MyAsyncTask(this).execute();
+    }
 
+    @Override
+    public void onResponse(@Nullable List<MostPopular> mostpopularList) {
+        Toast.makeText(getContext(), "Sucess", Toast.LENGTH_LONG).show();
+        Log.i("test", "blalba" + mostpopularList);
+        ArrayList<MostPopular> mostpopularListArrayList = new ArrayList<>(mostpopularList);
 
-        InfoCellAdapter mondapteur;
-        RecyclerView recyclerView = result.findViewById(R.id.recycleview_view);
-        mondapteur = new InfoCellAdapter(mostpopularlist);
-        recyclerView.setAdapter(mondapteur);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(KEYLIST, mostpopularListArrayList);
 
     }
 
+    @Override
+    public void onFailure() {
+        Toast.makeText(getContext(),"Failed", Toast.LENGTH_LONG).show();
+        Log.i("test", "failed");
+
+    }
+    public void executeHttpRequestWithRetrofit(){
+        CellInformationCalls.fetchMostPopularList(this);
+    }
+
+    private void configureHandlerThread(){
+        handlerThread = new MyHandlerThread("MyAwesomeHanderThread", this.progressBar);
+    }
+
+    private void startHandlerThread(){
+        handlerThread.startHandler();
+    }
+
+    @Override
+    public void onDestroy() {
+        // 3 - QUIT HANDLER THREAD (Free precious resources)
+        handlerThread.quit();
+        super.onDestroy();
+    }
+
+
+    @Override
+    public void onPreExecute() {
+        progressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    @Override
+    public void doInBackground() {
+
+    }
+
+    @Override
+    public void onPostExecute(Long success) {
+        progressBar.setVisibility(View.INVISIBLE);
+
+    }
 }
